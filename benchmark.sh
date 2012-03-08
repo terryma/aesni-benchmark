1d6154129f37ad90e3807e1da1ddeb41aa0cb43e
Terry Ma <zhenchuan.ma@gmail.com>
2012-03-08 00:56:32 +0000

    incremental

--- benchmark.sh
+++ benchmark.sh
@@ -1,26 +1,43 @@
 #!/bin/sh
 names=( plain aesni )
-count=300
+files=( /benchmark/test /benchmark-encrypted/test )
+length=${#names[@]}
+count=$1
 
-for name in ${names[@]}
+if [ -z $count ]
+then
+  count=300
+fi
+
+# create a directory for this test run
+dir=`date +"%Y_%m_%d_%H_%M"`
+mkdir $dir
+cd $dir
+
+for (( i=0; i<$length; i++ ));
 do
-  echo "Started $name test on `date`"
-  cd $name
-  # clean up the outputs
-  rm -rf *.log
-  rm -rf *.out
+  name=${names[$i]}
+  file=${files[$i]}
+
+  # copy the template input file
+  cp ../template.fio ./$name.fio  
   
   # set the time interval for fio
   sed -i s/^runtime=.*/runtime=$count/ $name.fio
+
+  # set the test name
+  sed -i s/^\\[-/\\[$name-/ $name.fio
+
+  # set the file
+  sed -i s,^filename=.*,filename=$file, $name.fio
   
   # start sar that captures CPU and device activity per second
   sar -u -P ALL -p -d -o $name-sar.out 1 $count >/dev/null 2>&1 &
+  echo "Started $name test on `date`"
   fio --output=$name-fio.out $name.fio >/dev/null 2>&1 
   echo "Stopped $name test on `date`"
   
   # convert the sar output
   sadf -d $name-sar.out -- -P ALL > $name-sar-cpu.out
   sadf -d $name-sar.out -- -d -p > $name-sar-disk.out
-
-  cd ..
 done
